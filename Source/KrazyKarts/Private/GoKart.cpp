c5758e8b6dc8c950b6cce60b9e33bf37fb1988d2
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-18 13:47:55 +0300

    AutonomousProxy vs SimulatedProxy

--- Source/KrazyKarts/Private/GoKart.cpp
+++ Source/KrazyKarts/Private/GoKart.cpp
@@ -4,6 +4,7 @@
 #include "GoKart.h"
 #include "Engine/World.h"
 #include "Components/InputComponent.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 AGoKart::AGoKart()
@@ -20,6 +21,22 @@ void AGoKart::BeginPlay()
 	
 }
 
+FString GetEnumText(ENetRole Role)
+{
+	switch (Role)
+	{
+	case ROLE_None:
+		return "None";
+	case ROLE_SimulatedProxy:
+		return "SimulatedProxy";
+	case ROLE_AutonomousProxy:
+		return "AutonomousProxy";
+	case ROLE_Authority:
+		return "Authority";
+	default:
+		return "ERROR";
+	}
+}
 // Called every frame
 void AGoKart::Tick(float DeltaTime)
 {
@@ -32,15 +49,13 @@ void AGoKart::Tick(float DeltaTime)
 
 	FVector Acceleration = Force / Mass;
 
-	
-
-
-
 	Velocity = Velocity + Acceleration * DeltaTime;
 
 	ApplyRotation(DeltaTime);
 
 	UpdateLocationFromVelocity(DeltaTime);
+
+	DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(Role), this, FColor::White, DeltaTime);
 }
 
 FVector AGoKart::GetAirResistance()
@@ -83,9 +98,21 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::Server_MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::Server_MoveRight);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
 
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Throttle = Value;
+	Server_MoveForward(Value);
+}
+
+void AGoKart::MoveRight(float Value)
+{
+	SteeringThrow = Value;
+	Server_MoveRight(Value);
 }
 
 void AGoKart::Server_MoveForward_Implementation(float Value)
--- Source/KrazyKarts/Public/GoKart.h
+++ Source/KrazyKarts/Public/GoKart.h
@@ -54,6 +54,10 @@ private:
 
 	float SteeringThrow;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
 	UFUNCTION(Server, Reliable, WithValidation)
 	void Server_MoveForward(float Value);
 
