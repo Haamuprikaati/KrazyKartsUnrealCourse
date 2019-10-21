85a6353ad6310c1c508df271c6633dfed89b5611
Konsta Kontunen <konsta.kontunen@ubisoft.com>
2019-10-21 14:50:19 +0300

    Extracting a Movement Component

--- Source/KrazyKarts/Public/GoKart.h
+++ Source/KrazyKarts/Public/GoKart.h
@@ -4,26 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "GoKartMovementComponent.h"
 #include "GoKart.generated.h"
-
-
-USTRUCT()
-struct FGoKartMove
-{
-	GENERATED_USTRUCT_BODY()
-
-	UPROPERTY()
-	float Throttle;
-
-	UPROPERTY()
-	float SteeringThrow;
-
-	UPROPERTY()
-	float DeltaTime;
-
-	UPROPERTY()
-	float Time;
-};
 
 USTRUCT()
 struct FGoKartState
@@ -62,58 +44,25 @@ public:
 
 private:
 
-	void SimulateMove(const FGoKartMove& Move);
-
-	FGoKartMove CreateMove(float DeltaTime);
-
 	void ClearAcknowledgedMoves(FGoKartMove LastMove);
-
-	//Mass of the car in kg
-	UPROPERTY(EditAnywhere)
-	float Mass = 1000;
-
-	//The throttle applied to the car when the throttle is fully down in newtons
-	UPROPERTY(EditAnywhere)
-	float MaxDrivingForce = 10000;
-
-	//Minimum radius of the car turning circle at full lock (m)
-	UPROPERTY(EditAnywhere)
-	float MinTurningRadius = 10;
-
-	//Higher means more drag
-	UPROPERTY()
-	float DragCoefficient = 16;
-
-	//Higher means more drag
-	UPROPERTY()
-	float RollingResistanceCoefficient = 0.1;
 
 	UPROPERTY(ReplicatedUsing = OnRep_ServerState)
 	FGoKartState ServerState;
 
-	FVector Velocity;
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SendMove(FGoKartMove Move);
 
 	UFUNCTION()
 	void OnRep_ServerState();
-
-	float Throttle;
-
-	float SteeringThrow;
 
 	void MoveForward(float Value);
 
 	void MoveRight(float Value);
 
-	UFUNCTION(Server, Reliable, WithValidation)
-	void Server_SendMove(FGoKartMove Move);
-
-	FVector GetAirResistance();
-	FVector GetRollingResistance();
-
-	void ApplyRotation(float DeltaTime, float SteeringThrow);
-
-	void UpdateLocationFromVelocity(float DeltaTime);
-
 	TArray<FGoKartMove> UnacknowledgedMoves;
+
+	UPROPERTY(EditAnywhere)
+	UGoKartMovementComponent* MovementComponent;
+
 
 };
---
+++ Source/KrazyKarts/Public/GoKartMovementComponent.h
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GoKartMovementComponent.generated.h"
+
+USTRUCT()
+struct FGoKartMove
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	float Throttle;
+
+	UPROPERTY()
+	float SteeringThrow;
+
+	UPROPERTY()
+	float DeltaTime;
+
+	UPROPERTY()
+	float Time;
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KRAZYKARTS_API UGoKartMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGoKartMovementComponent();
+
+	void SimulateMove(const FGoKartMove& Move);
+
+	FGoKartMove CreateMove(float DeltaTime);
+
+	FVector GetVelocity() { return Velocity; };
+	void SetVelocity(FVector Val) { Velocity = Val; };
+
+	void SetThrottle(float Val) { Throttle = Val; };
+	void SetSteeringThrow(float Val) { SteeringThrow = Val; };
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	void ApplyRotation(float DeltaTime, float SteeringThrow);
+
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+	FVector GetAirResistance();
+	FVector GetRollingResistance();
+
+	//Mass of the car in kg
+	UPROPERTY(EditAnywhere)
+	float Mass = 1000;
+
+	//The throttle applied to the car when the throttle is fully down in newtons
+	UPROPERTY(EditAnywhere)
+	float MaxDrivingForce = 10000;
+
+	//Minimum radius of the car turning circle at full lock (m)
+	UPROPERTY(EditAnywhere)
+	float MinTurningRadius = 10;
+
+	//Higher means more drag
+	UPROPERTY()
+	float DragCoefficient = 16;
+
+	//Higher means more drag
+	UPROPERTY()
+	float RollingResistanceCoefficient = 0.1;
+
+	FVector Velocity;
+
+	float Throttle;
+
+	float SteeringThrow;
+		
+};
